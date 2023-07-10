011439284cce16e11575b5589786452f5a0023f4
dr.omartalat <dr.omartalat@gmail.com>
2023-07-10 16:39:35 -0700

    0x0A-python-inheritance: task 5, 6  => 'Done'

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+==============================
+Module with BaseGeometry class
+==============================
+"""
+
+
+
+class BaseGeometry:
+    """
+    an empty class BaseGeometry.
+    """
+
+    pass
