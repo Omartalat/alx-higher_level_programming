074675c19bf343e042f7f7789b8ae833c41df294
dr.omartalat <dr.omartalat@gmail.com>
2023-07-10 18:10:29 -0700

    0x0A-python-inheritance: task 7 => 'enhanced'

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,7 +18,7 @@ class BaseGeometry:
         """
 
         raise Exception('area() is not implemented')
-    
+
     def integer_validator(self, name, value):
         """
         validates value:
