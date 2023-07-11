75c0788b7925dfdc4dede25b0b25fcbbbc750320
dr.omartalat <dr.omartalat@gmail.com>
2023-07-11 09:59:29 -0700

    0x0B-python-input_output: task 0 => 'permission removed done'

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,9 @@ Module with function read_file
 
 
 def read_file(filename=""):
-    with open(f'{filename}', 'r', encoding="utf-8") as f:
+    """
+    a function that reads a text file (UTF8) and prints it to stdout
+    """
+
+    with open(f'{filename}', encoding="utf-8") as f:
         print(f.read(), end='')
