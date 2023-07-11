9dd1fc06ca19a57910ab1604f56b0a9468bc010c
dr.omartalat <dr.omartalat@gmail.com>
2023-07-10 18:59:05 -0700

    0x0A-python-inheritance: task 8, 9 => 'import fixed'

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,45 +1,13 @@
 #!/usr/bin/python3
 """
-==============================
-Module with BaseGeometry class
-==============================
-"""
-
-
-class BaseGeometry:
-    """
-     a class BaseGeometry with Public instance method: area
-     and Public instance method: integer_validator
-    """
-
-    def area(self):
-        """
-        raises an Exception with the message area() is not implemented
-        """
-
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """
-        validates value:
-        if value is not an integer: raise a TypeError exception,
-        with the message <name> must be an integer
-        if value is less or equal to 0: raise a ValueError exception
-        with the message <name> must be greater than 0
-        """
-
-        if type(value) != int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
-
-"""
 ==============================================================
 Module with a  class Rectangle that inherits from BaseGeometry
 (7-base_geometry.py).
 ==============================================================
 """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
