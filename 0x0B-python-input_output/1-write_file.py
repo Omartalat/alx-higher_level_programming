b3e7ff28d1effac83f81ce8baedebbe2b1804cb9
dr.omartalat <dr.omartalat@gmail.com>
2023-07-11 10:34:32 -0700

    0x0B-python-input_output: task 1 => 'first attempt'

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,9 +8,9 @@ Module with a function write_file
 
 def write_file(filename="", text=""):
     """
-    function that writes a string to a text file (UTF8) and returns the number of characters written:
+    function that writes a string to a text file (UTF8)
+    and returns the number of characters written:
     """
 
-    with open(f'{filename}', 'w', encoding = 'utf-8') as f:
+    with open(f'{filename}', 'w', encoding='utf-8') as f:
         return f.write(text)
-
