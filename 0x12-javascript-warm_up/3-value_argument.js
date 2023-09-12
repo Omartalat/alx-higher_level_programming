d54d3c7a72aa740b114aa4d63a2351ede12373ad
Dr_Omar <dr.omartalat@gmail.com>
2023-09-12 04:47:48 +0300

    0x12. JavaScript - Warm up => 1-multi_languages.js, 2-arguments.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
-const len = args.length;
-if (len === 0) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(args[0]);
+  console.log(process.argv[2]);
 }
