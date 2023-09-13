b744d024fb94789daf3c8882401417e1251f0ffe
Dr_Omar <dr.omartalat@gmail.com>
2023-09-13 21:45:57 +0300

    0x12-javascript-warm_up => 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 let str = '';
-for (let i = 0; i < args[0]; i++) {
-  for (let j = 0; j < args[0]; j++) {
-    str += 'X';
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < args[0]; i++) {
+    for (let j = 0; j < args[0]; j++) {
+      str += 'X';
+    }
+    str += '\n';
   }
-  str += '\n';
+  console.log(str.slice(0, -1));
 }
-console.log(str.slice(0, -1));
