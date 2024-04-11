1d03507a1401555fbe99ed2d03c400a3eae4904e
Omar <dr.omartalat@gmail.com>
2024-04-11 06:22:17 +0200

    6-square.js and 5-square.js are added

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0){
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
@@ -19,14 +19,14 @@ class Rectangle {
   }
 
   rotate () {
-    let x = this.width;
+    const x = this.width;
     this.width = this.height;
     this.height = x;
   }
 
   double () {
-    this.width += this.width
-    this.height += this.height
+    this.width += this.width;
+    this.height += this.height;
   }
 }
 
