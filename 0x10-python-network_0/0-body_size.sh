19677c6be8d2e90aa79d46e6210196d9573896a3
Omar <dr.omartalat@gmail.com>
2024-04-27 10:37:03 +0300

    0-body_size.sh first trial

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 URL"
+    exit 1
+fi
+
 curl -s "$1" | wc -c
