3400f374fa536d4ac1d39ef327a24d8ab7a78dab
Talat <dr.omartalat@gmail.com>
2024-07-05 05:55:00 -0700

    0-hbtn_status.py no needed $ in first line removed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     page = response.read()
-    print('Body response:$')
+    print('Body response:')
     print('\t- type: {}'.format(type(page)))
     print('\t- content: {}'.format(page))
     print('\t- utf8 content: {}'.format(page.decode('utf-8')))
