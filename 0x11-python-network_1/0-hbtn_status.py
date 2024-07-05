3dd594329fc924047738d62133441d49b2d7d674
Talat <dr.omartalat@gmail.com>
2024-07-05 05:50:11 -0700

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request 
+
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    page = response.read()
+    print('Body response:$')
+    print('\t- type: {}'.format(type(page)))
+    print('\t- content: {}'.format(page))
+    print('\t- utf8 content: {}'.format(page.decode('utf-8')))
