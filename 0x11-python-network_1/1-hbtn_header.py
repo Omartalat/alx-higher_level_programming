030ba9c0cd96076dacdc510ec7b49ffc3bd5549f
Talat <dr.omartalat@gmail.com>
2024-07-05 06:38:24 -0700

    if __name__ == __main__: added

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,7 @@ a Python script that takes in a URL, and displays the value of X-Request-Id.
 import urllib.request
 import sys
 
-
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    print(response.getheader('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.getheader('X-Request-Id'))
