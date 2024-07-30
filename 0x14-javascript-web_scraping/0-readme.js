746e6f915c4676ba7e61066537b733cc15d2f065
Omar <dr.omartalat@gmail.com>
2024-07-31 00:21:59 +0300

    0-readme.js semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 const { argv } = require('node:process');
 
 fs.readFile(`./${argv[2]}`, 'utf-8', (err, data) => {
-	if (err) throw err;
-	console.log(data);
-})
+  if (err) throw err;
+  console.log(data);
+});
