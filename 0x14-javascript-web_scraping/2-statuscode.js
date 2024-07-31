84c9938f6550739c551713ea07f15f2ec6bd68d4
Omar <dr.omartalat@gmail.com>
2024-07-31 09:22:15 +0300

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (err, res, body) => {
   if (err) console.log(err);
-  console.log(`code ${res.statusCode}`);
+  console.log(`code: ${res.statusCode}`);
 });
