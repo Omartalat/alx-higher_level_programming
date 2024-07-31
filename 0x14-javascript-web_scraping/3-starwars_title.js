15bac0b6624c72efd044a599c6cbe2517f21dc51
Omar <dr.omartalat@gmail.com>
2024-07-31 09:38:49 +0300

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require("request");
+const movieID = process.argv[2];
+const url = "https://swapi-api.alx-tools.com/api/films/";
+const apiURL = url + movieID;
+
+request(apiURL, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    body = JSON.parse(body);
+    console.log(body.title);
+  }
+});
