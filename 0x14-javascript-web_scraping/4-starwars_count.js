685e61863f3218a5a2c99192d283978b183e19d6
Omar <dr.omartalat@gmail.com>
2024-07-31 10:13:56 +0300

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-const key = 'https://swapi-api.alx-tools.com/api/people/18/';
 const url = process.argv[2];
 let num = 0;
 
@@ -13,7 +12,7 @@ request(url, (err, _, body) => {
       const results = body.results;
       for (let i = 0; i < results.length; i++) {
         for (let j = 0; j < results[i].characters.length; j++) {
-          if (results[i].characters[j] === key) num++;
+          if (results[i].characters[j].endsWith('/18/')) num++;
         }
       }
     }
