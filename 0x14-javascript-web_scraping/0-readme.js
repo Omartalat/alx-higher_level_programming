f48cf819ab01aa6fcd96b966d4dd6119b1380403
Omar <dr.omartalat@gmail.com>
2024-07-31 07:32:14 +0300

    1-writeme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 const { argv } = require('node:process');
-
-fs.readFile(`./${argv[2]}`, 'utf-8', (err, data) => {
+fs.readFile(`./${argv[2]}`, 'utf8', (err, data) => {
   console.log(err || data);
 });
